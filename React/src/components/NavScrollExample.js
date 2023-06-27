f30f42ca2d46bcfcefa46243b9c5cf6f5853bf88
Maria Clara de Oliveira <mariaclaraminion@gmail.com>
2023-06-27 15:26:33 -0300

    Atualização hover e buttons
    hovers da nav bar com cores certas e buttons com active na cor certa

--- React/src/components/NavScrollExample.js
+++ React/src/components/NavScrollExample.js
@@ -9,36 +9,32 @@ import Stack from 'react-bootstrap/Stack';
 
 function NavScrollExample() {
   return (
-    <Navbar expand="lg" className='bg-body-tertiary'>
-      <Container fluid>
+      <>
+        <Navbar className='bg-body-tertiary'>
+          <Container fluid>
           <Navbar.Brand href="#">
-          <img
-                src={logo}
-                className="d-inline-block align-top ps-5"
-                alt="React Bootstrap logo"
-              />
-          </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll" className=''>
-          <Nav
-            className="ms-auto font_nav espacamento_nav itens_nav pe-5"
-            style={{ maxHeight: '70px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Início</Nav.Link>
-            <Nav.Link href="#action2">Aventure-se</Nav.Link>
-            <Nav.Link href="#action3">Origem</Nav.Link>
-            <Nav.Link href="#action4">Sobre nós</Nav.Link>
-            <Nav.Link href="#action5">Contato</Nav.Link>   
-          </Nav>
-        </Navbar.Collapse>
-        <div className='pe-5'>
-        <Button className="botao" href="/login">FAZER LOGIN</Button>
-        </div>
-        
-      </Container>
-    </Navbar>
-  );
-}
+            <img
+                  src={logo}
+                  className="d-inline-block align-top"
+                  alt="React Bootstrap logo"
+                />
+            </Navbar.Brand>
+            <Nav
+              className="font_nav espacamento_nav itens_nav"
+              style={{ maxHeight: '70px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Início</Nav.Link>
+              <Nav.Link href="#action2">Aventure-se</Nav.Link>
+              <Nav.Link href="#action3">Origem</Nav.Link>
+              <Nav.Link href="#action4">Sobre nós</Nav.Link>
+              <Nav.Link href="#action5">Contato</Nav.Link>
+              <Button className="botao" href="/login">FAZER LOGIN</Button>   
+            </Nav>
+          </Container>
+          </Navbar>
+      </>
+      );
+    }
 
 export default NavScrollExample;

23b869bd093efec9c55b9d5a7fd4ace11a7c9819
Maria Clara de Oliveira <mariaclaraminion@gmail.com>
2023-06-29 11:18:12 -0300

    Seção 1 - Landpage
    Adição conteúdo seção 1 landpage (sem responsividade)

--- React/src/components/LandingPage/Inicio.js
+++ React/src/components/LandingPage/Inicio.js
@@ -1,12 +1,24 @@
-//Hooks
-import {useNavigate} from 'react-router-dom';
-import { useState } from 'react';
+import adesivoQuests from "../imagens/adesivoQuests.svg"; 
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/esm/Col';
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
 
 const Inicio = () => {
-    return <div className='ps-5 pe-5'>
-            <h1>Bem vindo ao Quests</h1>
-            <p>Por: linos.....</p>
-        </div>
+    return (
+    <div className='ps-5'>
+        <Row className='pt-5 pb-3'>
+            <Col className='pt-5 pb-3'>
+                <h1 className='pt-5 pb-3 titulo_land'>A melhor maneira de responder questionários</h1>
+                <p className='text_land'>Lorem Ipsum is simply dummy text of the prin and typesetting industry. Lorem Ipsum hais be the industry's standard dummy text.</p>
+                <Button className="botao" href="/login">CADASTRE-SE JÁ</Button>
+            </Col>
+            <Col className='pb-5'>
+                <img src={adesivoQuests} className="img-fluid"></img>
+            </Col>
+        </Row>
+    </div>
+    )
 }
 
 export default Inicio;

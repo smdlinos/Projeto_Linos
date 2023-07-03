7ddad34f1984e2510b785d6f6c27a1b418afd6e4
Mickael Castro <mickaelcastro15@gmail.com>
2023-07-03 15:19:34 -0300

    Adicionei o svg do tabuleiro

--- React/src/pages/Tabletop.js
+++ React/src/pages/Tabletop.js
@@ -2,6 +2,7 @@
 import "../components/Tela.css"
 import Container from "react-bootstrap/esm/Container";
 import FundoTabletop from "../components/imagens/FundoTabletop.png"
+import Tabuleiro from "../components/imagens/Tab.svg"
 
 //Components
 import Header_q from "../components/Quests/Header_q"
@@ -10,10 +11,11 @@ import Footer from "../components/Global/Footer";
 
 function Tabletop(){
     return(
-        <div className="container" >
+        
+        <div className="background_gradient" >
             <Header_q/>
-            <Container className="background_gradient">
-            <h1 className='pb-3 titulo_login alinhamento'>Meu tabuleiro!</h1>
+            <Container className=" container">
+            <img src={Tabuleiro} alt="tabuleiro" className="ms-5 ps-4"/>
             </Container>
             <Footer/>
         </div>

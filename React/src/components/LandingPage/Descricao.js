5178dc9bf9d10393b5e8f870a9621a48515cacf0
Maria Clara de Oliveira <mariaclaraminion@gmail.com>
2023-07-04 12:52:51 -0300

    Seção 4 Cliente

--- React/src/components/LandingPage/Descricao.js
+++ React/src/components/LandingPage/Descricao.js
@@ -7,17 +7,15 @@ const Descricao = () => {
     return (
         <div>
             <Row className='pt-5 pb-3 sobreposicao_desc'>
-                <Col className='pt-5 pb-3' >
-                <img src={lina_desc} className="img-fluid"></img>
-                </Col>
-                <Col xs ={4}>
-                <img src={fala_lina_desc} className="img-fluid"></img>
-                </Col>
-                <Col xs ={6} className='pt-5 pb-3'>
-                <h1 className='pt-5 pb-3 titulo_land'>Entre nessa aventura!</h1>
-                <p className='text_land pe-5'>
+            {/* <p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p> 
+             ajeitar isso */}
+                <Col xs ={5}></Col>
+                <Col xs ={7} className='pt-5 pb-3'>
+                <h1 className='pt-5 pb-3 titulo_land_desc'>Entre nessa aventura!</h1>
+                <p className='text_land_desc pe-5'>
                 Prepare-se para uma incrível jornada rumo à recompensas! Embarque com a Lina e agarre o mapa de uma misteriosa floresta onde você vai superar obstáculos e contar com a companhia de corajosos aliados que estarão ao seu lado a cada passo. Não perca tempo, vá em busca dos cobiçados prêmios que te aguardam!
                 </p></Col>
+                {/*<p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p><p></p>*/}
             </Row>
         </div>
     )

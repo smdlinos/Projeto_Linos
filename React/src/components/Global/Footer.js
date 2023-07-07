c63dcaeb8c54d7773fd1fe8c9d4ad8d12cd3f48f
Mickael Castro <mickaelcastro15@gmail.com>
2023-07-07 14:41:30 -0300

    tela deletar

--- React/src/components/Global/Footer.js
+++ React/src/components/Global/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
   <div>
     
-      <div className="Footer pt-3 fonte_login alinhamento">
+      <div className="Footer pt-3 fonte_login alinhamento background_footer">
         <p>© 2023 Feito com <img src={cor} alt="coracao"/> pela Linos.</p>
       </div>
 
--- React/src/pages/QuestsDelete.js
+++ React/src/pages/QuestsDelete.js
@@ -7,6 +7,7 @@ import SessionLogin from "../components/Login/SessionLogin";
 import Header from "../components/Global/Header"
 import Footer from "../components/Global/Footer";
 import Delete from "../components/imagens/Tela_Delete.svg"
+import Container from "react-bootstrap/esm/Container";
 
 
 
@@ -14,15 +15,16 @@ import Delete from "../components/imagens/Tela_Delete.svg"
 function QuestsDelete (){
     
     return(
-        <div className="body">
-
+        <div className="body ">
+            <Container className="tela_deletar">
             <div className="img_delete">
-            <img src={Delete} alt="deleta"/>
+            <img src={Delete} alt="deleta" className=""/>
                 <div className="link_home">
                     <a >aaaaaaaa</a>
                 </div>
             </div>
-            
+
+            </Container>
 
         </div>
     )
